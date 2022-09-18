d86d80ea6046d1b74fd533151324eab682554d75
hheeju98 <hheeju98@naver.com>
2022-09-18 23:15:27 +0900

    비밀번호 해시

--- src/main/java/site/metacoding/instagram/service/AuthService.java
+++ src/main/java/site/metacoding/instagram/service/AuthService.java
@@ -1,5 +1,8 @@
 package site.metacoding.instagram.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -11,9 +14,15 @@ import site.metacoding.instagram.domain.user.UserRepository;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Transactional // Write(Insert, Update, Delete)
     public User 회원가입(User user) {
         // 회원가입 진행
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole("ROLE_USER"); // 관리자 ROLE_ADMIN
         User userEntity = userRepository.save(user);
         return userEntity;
     }
--- src/main/java/site/metacoding/instagram/web/AuthController.java
+++ src/main/java/site/metacoding/instagram/web/AuthController.java
@@ -1,8 +1,7 @@
 package site.metacoding.instagram.web;
 
-import java.util.logging.Logger;
-
-import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +14,8 @@ import site.metacoding.instagram.web.dto.auth.SignupDto;
 @RequiredArgsConstructor // final 필드를 DI 할때 사용
 @Controller // 1.IoC 2.파일을 리턴하는 컨트롤러
 public class AuthController {
-    private static final org.jboss.logging.Logger log = LoggerFactory.logger(AuthController.class);
+
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 
     private final AuthService authService;
 
@@ -25,23 +25,23 @@ public class AuthController {
 
     @GetMapping("/auth/signin")
     public String signinForm() {
-        return "auth/signin";
+        return "/auth/signin";
     }
 
     @GetMapping("/auth/signup")
     public String signupForm() {
-        return "auth/signup";
+        return "/auth/signup";
     }
 
     // 회원가입 버튼 -> /auth/signup -> /auth/signin
     @PostMapping("/auth/signup")
     public String signup(SignupDto signupDto) { // key=value (x-www-form-urlencoded)
-        log.info(signupDto.toString());
+        // log.info(signupDto.toString());
         // User < - SignupDto
         User user = signupDto.toEntity();
         User userEntity = authService.회원가입(user);
         System.out.println(userEntity);
         // log.info(user.toString());
-        return "auth/signin";
+        return "/auth/signin";
     }
 }
