05e88777129d3e99cc886a6f781c3c9dd9d54410
hheeju98 <hheeju98@naver.com>
2022-09-19 00:54:11 +0900

    회원가입 -유효성 검사

--- src/main/java/site/metacoding/instagram/web/dto/auth/SignupDto.java
+++ src/main/java/site/metacoding/instagram/web/dto/auth/SignupDto.java
@@ -1,13 +1,20 @@
 package site.metacoding.instagram.web.dto.auth;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 import site.metacoding.instagram.domain.user.User;
 
 @Data // Getter, Setter
 public class SignupDto {
+    @Max(20)
     private String username;
+    @NotBlank
     private String password;
+    @NotBlank
     private String email;
+    @NotBlank
     private String name;
 
     public User toEntity() {
