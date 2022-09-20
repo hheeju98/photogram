a82f259566e4b979a6fb757df87f7f40b0bb75a3
hheeju98 <hheeju98@naver.com>
2022-09-21 00:09:17 +0900

    공통응답 Script 만들기

--- src/main/java/site/metacoding/instagram/handler/ControllerExceptionHandler.java
+++ src/main/java/site/metacoding/instagram/handler/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import site.metacoding.instagram.handler.ex.CustomValidationException;
+import site.metacoding.instagram.util.Script;
 import site.metacoding.instagram.web.dto.auth.CMRespDto;
 
 @RestController
@@ -15,7 +16,12 @@ public class ControllerExceptionHandler {
 
     // 모든 runtimeException가로챔
     @ExceptionHandler(CustomValidationException.class)
-    public CMRespDto<?> validationException(CustomValidationException e) {
-        return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
+    public String validationException(CustomValidationException e) {
+
+        // CMRespDto, Script 비교
+        // 1. 클라이언트에게 응답 할때는 script좋음. (클라이언트가 응답 받음)
+        // 2. Ajax통신 - CMRespDto (개발자가 응답받음 - 코드로 받아서 처리)
+        // 3. Android 통신 - CMRespDto (개발자가 응답받음)
+        return Script.back(e.getErrorMap().toString());
     }
 }
