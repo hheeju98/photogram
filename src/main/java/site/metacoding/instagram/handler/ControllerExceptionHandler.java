aa6b8fbb698c0072a32ec1a267efaf1d99e22380
hheeju98 <hheeju98@naver.com>
2022-09-20 22:38:45 +0900

    공통응답 DTO 만들기

--- src/main/java/site/metacoding/instagram/handler/ControllerExceptionHandler.java
+++ src/main/java/site/metacoding/instagram/handler/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import site.metacoding.instagram.handler.ex.CustomValidationException;
+import site.metacoding.instagram.web.dto.auth.CMRespDto;
 
 @RestController
 @ControllerAdvice
@@ -14,7 +15,7 @@ public class ControllerExceptionHandler {
 
     // 모든 runtimeException가로챔
     @ExceptionHandler(CustomValidationException.class)
-    public Map<String, String> validationException(CustomValidationException e) {
-        return e.getErrorMap();
+    public CMRespDto<?> validationException(CustomValidationException e) {
+        return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
     }
 }
---
+++ src/main/java/site/metacoding/instagram/web/dto/auth/CMRespDto.java
@@ -0,0 +1,16 @@
+package site.metacoding.instagram.web.dto.auth;
+
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CMRespDto<T> {
+    private int code; // 1(성공), -1(실패)
+    private String message;
+    private T data;
+}
