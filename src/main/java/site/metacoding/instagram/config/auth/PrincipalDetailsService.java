c474922335204afedf519d47b9a5255245c4ca59
hheeju98 <hheeju98@naver.com>
2022-09-21 23:36:30 +0900

    로그인(완료)

--- src/main/java/site/metacoding/instagram/config/auth/PrincipalDetailsService.java
+++ src/main/java/site/metacoding/instagram/config/auth/PrincipalDetailsService.java
@@ -5,13 +5,28 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.instagram.domain.user.User;
+import site.metacoding.instagram.domain.user.UserRepository;
+
+@RequiredArgsConstructor
 @Service // IoC
 public class PrincipalDetailsService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
+    // 1. 패스워드는 알아서 체킹하니까 신경쓸 필요 없다.
+    // 2. 리턴이 잘되면 자동으로 UserDetails타입을 세션으로 만든다.
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println("-----------실행돼?");
-        return null;
+
+        User userEntity = userRepository.findByUsername(username);
+        if (userEntity == null) {
+            return null;
+        } else {
+            return new PrincipalDetails(userEntity);
+        }
+
     }
 
 }
