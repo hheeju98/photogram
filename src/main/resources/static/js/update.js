4f4ff04d75ab96a172027e01c89622321d7e4f7a
hheeju98 <hheeju98@naver.com>
2022-09-24 23:33:36 +0900

    ajax 오류수정

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -9,11 +9,11 @@ function update(userId, event) {
         type:"put",
         url: `/api/user/${userId}`,
         data: data,
-        contentType: "application/x-www-form-urlencoded: charset=utf-8",
+        contentType: "application/x-www-form-urlencoded; charset=utf-8",
         dataType: "json"
     }).done(res=>{
-console.log("update 성공");
+console.log("update 성공", res);
     }).fail(error=>{
-console.log("update 실패");
+console.log("update 실패", error);
     });
 }
