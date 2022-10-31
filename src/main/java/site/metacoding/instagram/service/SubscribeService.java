32997c1132c65904359f78a73859a9992dd7f543
hheeju98 <hheeju98@naver.com>
2022-10-31 14:43:12 +0900

    구독 모달 정보를 위한 API만들기

--- src/main/java/site/metacoding/instagram/service/SubscribeService.java
+++ src/main/java/site/metacoding/instagram/service/SubscribeService.java
@@ -1,18 +1,26 @@
 package site.metacoding.instagram.service;
 
-import javax.transaction.Transactional;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.instagram.domain.subscribe.SubscribeRepository;
 import site.metacoding.instagram.handler.ex.CustomApiException;
+import site.metacoding.instagram.web.dto.subscribe.SubscribeDto;
 
 @RequiredArgsConstructor
 @Service
 public class SubscribeService {
 
     private final SubscribeRepository subscribeRepository;
+
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
+    public List<SubscribeDto> 구독리스트(int principalId, int pageUserId) {
+
+        return null;
+    }
 
     @Transactional // insert, delete를 통해 디비에 영향을 준다.
     public void 구독하기(int fromUserId, int toUserId) {
--- src/main/java/site/metacoding/instagram/web/api/UserApiController.java
+++ src/main/java/site/metacoding/instagram/web/api/UserApiController.java
@@ -1,13 +1,17 @@
 package site.metacoding.instagram.web.api;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +20,10 @@ import lombok.RequiredArgsConstructor;
 import site.metacoding.instagram.config.auth.PrincipalDetails;
 import site.metacoding.instagram.domain.user.User;
 import site.metacoding.instagram.handler.ex.CustomValidationApiException;
+import site.metacoding.instagram.service.SubscribeService;
 import site.metacoding.instagram.service.UserService;
 import site.metacoding.instagram.web.dto.auth.CMRespDto;
+import site.metacoding.instagram.web.dto.subscribe.SubscribeDto;
 import site.metacoding.instagram.web.dto.user.UserUpdateDto;
 
 @RequiredArgsConstructor
@@ -25,6 +31,16 @@ import site.metacoding.instagram.web.dto.user.UserUpdateDto;
 public class UserApiController {
 
     private final UserService userService;
+    private final SubscribeService subscribeService;
+
+    @GetMapping("/api/user/1/{pageUserId}/subscribe")
+    public ResponseEntity<?> subscribeList(@PathVariable int pageUserId,
+            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        List<SubscribeDto> subscribeDto = subscribeService.구독리스트(principalDetails.getUser().getId(), pageUserId);
+
+        return new ResponseEntity<>(new CMRespDto<>(1, "구독자 정보 리스트 가져오기 성공", subscribeDto), HttpStatus.OK);
+    }
 
     @PutMapping("/api/user/{id}")
     public CMRespDto<?> update(
